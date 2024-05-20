936f9537aa8c3fdeef17a77a08c69b240b97e6a1
imran-khan-rehan <imran.liaqat1925@gmail.com>
2024-05-20 16:33:01 +0500

    fds

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Update this with your frontend origin
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
